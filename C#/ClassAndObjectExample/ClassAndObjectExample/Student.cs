e2ec1e8e2333617d58d9877ea5825afcfb933118
lacie-life <00sao00ios00@gmail.com>
2021-09-02 01:07:57 +0700

    [Update] Class and Object Example

---
+++ C#/ClassAndObjectExample/ClassAndObjectExample/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndObjectExample
+{
+    class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            this.name = aName;
+            this.major = aMajor;
+            this.gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            if (gpa >= 4.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ C#/ClassAndObjectExample/ClassAndObjectExample/Tools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndObjectExample
+{
+    class Tools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello" + name);
+        }
+    }
+}
